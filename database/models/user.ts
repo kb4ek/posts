a5fa3c5931200d92ab0be08982eac4a9e5a8c218
kb4ek <kbaek1202@gmail.com>
2019-05-08 00:14:20 +0900

    user-system_database

---
+++ database/models/user.ts
@@ -0,0 +1,40 @@
+import {
+  AllowNull,
+  AutoIncrement,
+  BelongsTo,
+  Column,
+  CreatedAt,
+  DataType,
+  ForeignKey,
+  Model,
+  PrimaryKey,
+  UpdatedAt,
+  Table,
+} from 'sequelize-typescript';
+
+@Table({
+  timestamps: true,
+})
+export default class User extends Model<User> {
+  @PrimaryKey
+  @Column(DataType.INTEGER)
+  public pk: number;
+
+  @AllowNull(false)
+  @Column(DataType.STRING)
+  public userID: string;
+
+  @AllowNull(false)
+  @Column(DataType.STRING)
+  public password: string;
+
+  @AllowNull(false)
+  @Column(DataType.STRING)
+  public name: string;
+
+  @CreatedAt
+  public createdAt: Date;
+
+  @UpdatedAt
+  public updatedAt: Date;
+}
