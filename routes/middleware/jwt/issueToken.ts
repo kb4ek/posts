d763df088c487bb585039cc38e1f67f55134b9d5
kb4ek <kbaek1202@gmail.com>
2019-05-09 22:43:43 +0900

    login 성공 시 토큰 발급

---
+++ routes/middleware/jwt/issueToken.ts
@@ -0,0 +1,29 @@
+import * as jwt from 'jsonwebtoken';
+import { NextFunction, Request, Response } from 'express';
+
+import User from '../../../database/models/user';
+
+const issueToken = (req: Request, res: Response, next: NextFunction) => {
+  const user: User = res.locals.user;
+  const tokenSecret = process.env.TOKEN_SECRET;
+  const token = jwt.sign({ token: user.userID }, tokenSecret);
+
+  res.status(200).json({
+    result: {
+      SUCCESS: true,
+      message: '로그인 성공',
+    },
+    data: {
+      token: token,
+      user: {
+        pk: user.pk,
+        userID: user.userID,
+        password: user.password,
+        passwordKey: user.passwordKey,
+        name: user.name,
+      },
+    },
+  });
+};
+
+export default issueToken;
