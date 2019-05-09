5a98d9a4e02263a120fcdb10da8051d026e496f8
kb4ek <kbaek1202@gmail.com>
2019-05-09 22:42:12 +0900

    user 정보 확인 - 로그인, 회원가입 통합

---
+++ routes/middleware/common/findUser.ts
@@ -0,0 +1,35 @@
+import { NextFunction, Request, Response } from 'express';
+
+import User from '../../../database/models/user';
+
+const findUser = async (req: Request, res: Response, next: NextFunction) => {
+  try {
+    const { id } = req.body;
+
+    await User.findOne({
+      where: { userID: id },
+    }).then((user: User) => {
+      switch (req.path) {
+        case '/register':
+          if (user) {
+            res.status(400).json({ result: { SUCCESS: false, message: '중복된 아이디' } });
+          } else {
+            next();
+          }
+          break;
+        case '/login':
+          if (user) {
+            res.locals.user = user;
+            next();
+          } else {
+            res.status(400).json({ result: { SUCCESS: false, message: '존재하지 않는 아이디' } });
+          }
+      }
+    });
+  } catch (findErr) {
+    console.log(findErr);
+    res.status(500).json({ result: { SUCCESS: false, message: '서버 에러' } });
+  }
+};
+
+export default findUser;
