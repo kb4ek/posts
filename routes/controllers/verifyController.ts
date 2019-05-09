67e0e079f41eba9e640d0f9f877bd51f8a0fb713
kb4ek <kbaek1202@gmail.com>
2019-05-09 22:52:06 +0900

    login API 추가

--- routes/controllers/verifyController.ts
+++ routes/controllers/verifyController.ts
@@ -1,20 +1,27 @@
 import { Router } from 'express';
 
-//validation
-import joinValidation from '../middleware/register/_validation';
+//validation, common
+import LoginValidation from '../middleware/login/_validation';
+import JoinValidation from '../middleware/register/_validation';
 import checkValidation from '../middleware/common/checkValidation';
+import findUser from '../middleware/common/findUser';
+
+//login
+import login from '../middleware/login/login';
+import issueToken from '../middleware/jwt/issueToken';
 
 //register
-import findUser from '../middleware/register/findUser';
 import passwordEncryption from '../middleware/common/passwordEncryption';
 import createUser from '../middleware/register/createUser';
 
 const router = Router();
 
-router.post('/register', joinValidation);
+router.post('/login', LoginValidation);
+router.post('/register', JoinValidation);
 
 router.use(checkValidation);
 
+router.post('/login', findUser, passwordEncryption, login, issueToken);
 router.post('/register', findUser, passwordEncryption, createUser);
 
 export default router;
