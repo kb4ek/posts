5f77a1c63d9d2d2b0b7922c365702aee1b85a6a7
kb4ek <kbaek1202@gmail.com>
2019-05-11 00:06:39 +0900

    글 작성 시 데이터 형식 검증(미완)

---
+++ routes/middleware/post/_validation.ts
@@ -0,0 +1,31 @@
+import { body, ValidationChain } from 'express-validator/check';
+import { title, contents } from '../../../config/regexp.json';
+
+const PostValidation: ValidationChain[] = [
+  body('title').isString(),
+  /* .custom((value, { req }) => {
+      const titleRegExp: RegExp = new RegExp(title);
+
+      if (req.body.title) {
+        if (req.body.title === 'title') {
+          return titleRegExp.test(value);
+        }
+      } else {
+        return false;
+      }
+    }), */
+  body('contents').isString(),
+  /* .custom((value, { req }) => {
+      const contentsRegExp: RegExp = new RegExp(contents);
+
+      if (req.body.contents) {
+        if (req.body.contents === 'contents') {
+          return contentsRegExp.test(value);
+        }
+      } else {
+        return false;
+      }
+    }), */
+];
+
+export default PostValidation;
