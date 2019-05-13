7aeaf823e6b3ab83bbe1cc4f82feef81f66d65bf
kb4ek <kbaek1202@gmail.com>
2019-05-13 22:48:41 +0900

    폴더 위치 변경

--- routes/middleware/login/_validation.ts
+++
@@ -1,5 +0,0 @@
-import { body, ValidationChain } from 'express-validator/check';
-
-const LoginValidation: ValidationChain[] = [body('id').isString(), body('password').isString()];
-
-export default LoginValidation;
--- routes/middleware/register/_validation.ts
+++
@@ -1,5 +0,0 @@
-import { body, ValidationChain } from 'express-validator/check';
-
-const JoinValidation: ValidationChain[] = [body('id').isString(), body('password').isString(), body('name').isString()];
-
-export default JoinValidation;
